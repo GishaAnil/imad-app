97df50744c26319800b89d9ce549f4299f1f152b
GishaAnil <36534979+GishaAnil@users.noreply.github.com>
2018-02-21 13:03:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML='Gisha';
 
 var button = document.getElementById('counter');
 var counter=0;
-counter.onclick = function() {
+button.onclick = function() {
     counter=counter+1;
     var span = document.getElementById('count');
     span.innerHTML=counter.toString();
